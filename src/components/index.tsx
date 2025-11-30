c7d7d2faf07e32b9f9c5e35797050a44c85e7ce9
Hyun Seoyeon <tjdus4429@gachon.ac.kr>
2025-11-30 11:17:47 +0900

    [Feat] 뱃지 컴포넌트 구현

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/common/button/Button";
 import Image from "./common/image/Image";
 import Input from "./common/input/Input";
+import Badge from "./common/badge/Badge";
 
-export {Button, Image, Input};
+export {Button, Image, Input, Badge};
