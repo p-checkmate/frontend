b9dede348df46ad49f7e62e8080b8f94ea144fea
sispo3314 <sispo3314@gmail.com>
2025-11-30 20:23:19 +0900

    chore: 테스트 라우트 삭제

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,14 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/main/Homepage";
-import ModalTest from "@/components/test/ModalTest";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-  },
-  {
-    path: "/test",
-    element: <ModalTest />,
   },
 ]);
