e8cd693885ce8f85fa0034a773a51ebe6da350c8
sispo3314 <sispo3314@gmail.com>
2025-11-30 12:22:54 +0900

    refact: 토론 Input을 text-area로 수정

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/main/Homepage";
+import DebateDetailPage from "@/components/test/DebateTest";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/test",
+    element: <DebateDetailPage />,
   },
 ]);
