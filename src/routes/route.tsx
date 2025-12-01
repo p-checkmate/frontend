ff008bac19a27ac90fab0a19e0d3f9d159f09a85
sispo3314 <sispo3314@gmail.com>
2025-12-01 13:29:26 +0900

    chore: 테스트 코드 삭제

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,14 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/main/Homepage";
-import DiscussionCardTest from "@/components/test/CardTest";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-  },
-  {
-    path: "/test",
-    element: <DiscussionCardTest />,
   },
 ]);
